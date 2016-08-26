d578aaea8fd0c4dcfe1abe9949f2e9c8df31bfb1
FANRANRAN <ran.fan@tricubics.com>
2016-08-26 14:08:21 +0900

    commit

--- app/src/main/java/com/example/fanranran/one/MainActivity.java
+++ app/src/main/java/com/example/fanranran/one/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.fanranran.one;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView=(TextView)findViewById(R.id.textView01);
+
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
